3e91708412ebbbb6dfc242940d9c35184292be52
AUlasen <and3456@gmail.com>
2019-07-19 20:38:33 +0300

    Lesson24 test with browser

--- Lesson24/test_set.py
+++ Lesson24/test_set.py
@@ -1,10 +1,23 @@
+from selenium import webdriver
+from selenium.webdriver import ChromeOptions
+
+
 
 def test_1():
     assert True
 
 
 def test_2():
-    assert False
+    options = ChromeOptions()
+    options.add_argument("--start-fullscreen")
+    options.add_argument('--headless')
+
+    capabilities = webdriver.DesiredCapabilities.CHROME.copy()
+    capabilities['acceptSslCerts'] = True
+    capabilities['acceptInsecureCerts'] = True
+    wd = webdriver.Chrome(desired_capabilities=capabilities, options=options)
+    wd.get("https://ya.ru/")
+
 
 
 def test_3():
